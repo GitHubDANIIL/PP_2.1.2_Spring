a2410ab83994d763bd17018b592e753f23a266f1
GitHubDANIIL <129953722+GitHubDANIIL@users.noreply.github.com>
2023-07-19 20:01:08 +0300

    add Cat class and update AppConfig

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanHello =
+                (HelloWorld) applicationContext.getBean(("helloworld"));
+        System.out.println();
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("getCat");
+        System.out.println(catBean.getMeow());
+        Cat catBean1 =
+                (Cat) applicationContext.getBean(("getCat"));
+        System.out.println(catBean1.getMeow());
+        System.out.println("------------------------------------");
+
+        System.out.println("Сравнение бинов Cat : " + (catBean==catBean1));
+        System.out.println("Сравнение бинов HelloWorld : " + (bean==beanHello));
     }
 }
